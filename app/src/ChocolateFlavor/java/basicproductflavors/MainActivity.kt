e4365e53cd4b9a954e92e4fa4544db583f75438b
Muhamad Syafii <muhamadsyafii4@gmail.com>
2022-10-05 18:10:32 +0700

    final project (#1)

--- app/src/ChocolateFlavor/java/basicproductflavors/MainActivity.kt
+++ app/src/ChocolateFlavor/java/basicproductflavors/MainActivity.kt
@@ -1,14 +1,14 @@
-package com.syafii.basicproductflavors
+package basicproductflavors
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.syafii.basicproductflavors.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
